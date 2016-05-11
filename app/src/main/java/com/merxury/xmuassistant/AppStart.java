f7246353f2d54bbb011b68a052e200a9eb4bbaa8
lihenggui <350699171@qq.com>
2016-05-11 21:42:28 +0800

    添加应用程序启动界面（未完成）

---
+++ app/src/main/java/com/merxury/xmuassistant/AppStart.java
@@ -0,0 +1,51 @@
+package com.merxury.xmuassistant;
+
+/**
+ * Created by lihen on 2016/5/11.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+public class AppStart extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final View view = View.inflate(this, R.layout.start, null);
+        setContentView(view);
+
+        //渐变展示启动屏
+        AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
+        aa.setDuration(2000);
+        view.startAnimation(aa);
+        aa.setAnimationListener(new Animation.AnimationListener() {
+
+            public void onAnimationEnd(Animation arg0) {
+                redirectTo();
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+
+        });
+
+
+    }
+
+    /**
+     * 跳转到...
+     */
+    private void redirectTo() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
