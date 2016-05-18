9c4d532635f75876f11a5bdea61e2abe63605f24
dhd <609490912@qq.com>
2016-05-18 23:37:12 +0800

    主界面和分数查询

--- app/src/main/java/com/merxury/xmuassistant/MainActivity.java
+++ app/src/main/java/com/merxury/xmuassistant/MainActivity.java
@@ -112,17 +112,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_library) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_score) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_balance) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_setting) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_exit) {
 
         }
 
---
+++ app/src/main/java/com/merxury/xmuassistant/ObservableScrollView.java
@@ -0,0 +1,58 @@
+package com.merxury.xmuassistant;
+
+/**
+ * Created by dhd on 2016/5/18.
+ */
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+
+public class ObservableScrollView extends ScrollView {
+
+    public interface ScrollViewListener {
+
+        void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
+
+    }
+
+    private ScrollViewListener scrollViewListener = null;
+
+    public ObservableScrollView(Context context) {
+        super(context);
+
+    }
+
+
+    public ObservableScrollView(Context context, AttributeSet attrs,
+                                int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+
+    public ObservableScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+    public void setScrollViewListener(ScrollViewListener scrollViewListener) {
+        this.scrollViewListener = scrollViewListener;
+
+    }
+
+    @Override
+
+
+    protected void onScrollChanged(int x, int y, int oldx, int oldy) {
+        super.onScrollChanged(x, y, oldx, oldy);
+        if (scrollViewListener != null) {
+            scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
+
+        }
+
+    }
+
+
+}
